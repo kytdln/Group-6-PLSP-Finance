ec124e425f334c0852baf39f6f4d400ea9e7efb4
kytdln <deleon03keith@gmail.com>
2024-05-06 10:10:10 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Month', 'Earnings'],
+          ['Jan', 400],
+          ['Feb', 460],
+          ['Mar', 1120],
+          ['Apr', 540]
+        ]);
+
+        var options = {
+          curveType: 'function',
+          legend: { position: 'bottom' }
+        };
+
+        var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+
+        chart.draw(data, options);
+      }
